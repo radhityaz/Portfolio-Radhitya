cdb8851204b20b3ee9529dedb5bcab64c6df35cc
radhityaz <rosita.yuliati2@gmail.com>
2025-03-30 14:54:54 +0700

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles/index.css";
+
+const container = document.getElementById("root");
+if (!container) throw new Error("Could not find root element");
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
